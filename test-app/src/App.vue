1558519a095e9d63e973703e0ca7a910fd89b176
ZachyDev <mosetizachary001@gmail.com>
2020-12-16 15:14:09 +0300

    fetched data from Chuck Norris API

---
+++ test-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" style="width:300px;">
+    <Categories />
+  </div>
+</template>
+
+<script>
+import Categories from './components/Categories.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Categories,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ test-app/src/components/Categories.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="home">
+        <h1>Hey I'm Chuck Norris</h1>
+        <button class="btn btn-primary" @click="getRandomJoke">Random  Joke</button>
+        <div class="card" style="width: 18rem;margin-left:10px;margin-top:30px;" >
+            <img src="" class="card-img-top" alt="">
+            <div class="card-body" v-for="joke in chuckArray" :key="joke.id">
+              <h5 class="card-title"><strong>Joke</strong></h5>
+              <p class="card-text">{{ joke.value }}</p>
+              <a href="#" class="btn btn-primary" @click="clearJoke">Clear Joke</a>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+import axios  from 'axios';
+export default {
+  data() {
+    return {
+      chuckArray: [],
+    }
+  },
+  methods: {
+    getRandomJoke() {
+      axios.get('https://api.chucknorris.io/jokes/random')
+        .then(res => {
+          console.log(res.data);
+          this.chuckArray.push(res.data)
+        })
+        .catch(err => {
+          console.log(err.message)
+        })
+    },
+    clearJoke() {
+      this.chuckArray = []
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
